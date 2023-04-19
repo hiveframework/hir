044d7cf91941ea9eafe860ed4c1551f5e630d142
Anita Anderson <anita@ansan.io>
2023-04-18 22:17:02 -0400

    Started work on the hir parser
    Signed-off-by: Anita Anderson <anita@ansan.io>

---
+++ include/node/Node.hh
@@ -0,0 +1,41 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+
+#include <node/NodeKind.hh>
+
+class Node;
+
+class RegisterNode;
+class DataRegisterNode;
+class LiteralNode;
+
+class AddNode;
+class SubNode;
+class DivNode;
+class MulNode;
+class AndNode;
+class OrNode;
+class XorNode;
+class NotNode;
+class LabelNode;
+class CompareEqualityNode;
+class CompareLessThanNode;
+class CompareGreaterThanNode;
+class JumpIfNode;
+class JumpNotEqualNode;
+class JumpEqualNode;
+class FunctionNode;
+class ReturnNode;
+class DerefNode;
+class PointerToNode;
+class CallNode;
+class StoreNode;
+class WriteNode;
+class DebugNode;
+class DataStaticNode;
+class DataStructNode;
+class TypeNode;
